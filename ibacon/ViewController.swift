7ab6de226ebc21a757e5429f41959f41345c9774
Maximilian Fischer <mafi@inagile.org>
2015-11-04 15:44:36 +0100

    Initial Commit

---
+++ ibacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ibacon
+//
+//  Created by Maximilian Fischer on 04/11/15.
+//  Copyright © 2015 Maximilian Fischer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
